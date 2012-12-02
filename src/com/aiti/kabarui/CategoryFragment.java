3ae905ae0556ab307b98db93466bd5bfad7edace
maviosso <maviosso@maviosso-Rev-1-0>
2012-12-02 16:45:03 +0700

    Try implement xml fetch using technique taught from ITCuties

--- src/com/aiti/kabarui/CategoryFragment.java
+++ src/com/aiti/kabarui/CategoryFragment.java
@@ -1,75 +1,51 @@
 package com.aiti.kabarui;
- 
-import java.util.ArrayList;
-import java.util.HashMap;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.LinearLayout;
 import android.widget.ListView;
- 
+
+import com.aiti.kabarui.rss.RssReader;
+
 public class CategoryFragment extends Fragment {
-    static final String KEY_ID = "id";
-    static final String KEY_TITLE = "title";
-    static final String KEY_ARTIST = "artist";
-    static final String KEY_DURATION = "duration";
- 
-    ListView list;
-    NewsAdapter adapter;
- 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-    }
+	static final String KEY_ID = "id";
+	static final String KEY_TITLE = "title";
+	static final String KEY_ARTIST = "artist";
+	static final String KEY_DURATION = "duration";
+	RssReader rssReader;
+	ListView list;
+	NewsAdapter adapter;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		try {
+			// Create RSS reader
+			rssReader = new RssReader("http://www.anakui.com/category/beasiswa-lowongan/feed/");
+			// Get a ListView from main view
+
+		} catch (Exception e) {
+			Log.e("ITCRssReader", e.getMessage());
+		}
+	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
-		View v = inflater.inflate(R.layout.category, container,false);
-		 
-        ArrayList<HashMap<String, String>> songsList = new ArrayList<HashMap<String, String>>();
- 
-        // looping through all song nodes &lt;song&gt;
-        for (int i = 0; i < 7; i++) {
-            // creating new HashMap
-            HashMap<String, String> map = new HashMap<String, String>();
-            // adding each child node to HashMap key =&gt; value
-            map.put(KEY_ID, ""+i);
-            map.put(KEY_TITLE, "Judul Berita");
-            map.put(KEY_ARTIST, "Penulis" );
-            map.put(KEY_DURATION, "Tanggal" );
- 
-            // adding HashList to ArrayList
-            songsList.add(map);
-        }
- 
-        list=(ListView)v.findViewById(R.id.list_berita);
- 
-        // Getting adapter by passing xml data ArrayList
-        adapter=new NewsAdapter(getActivity(), songsList);
-        list.setAdapter(adapter);
- 
-        // Click event for single list row
-        list.setOnItemClickListener(new OnItemClickListener() {
+		View v = inflater.inflate(R.layout.category, container, false);
+		list = (ListView) v.findViewById(R.id.list_berita);
+		try {
+			adapter = new NewsAdapter(getActivity(), rssReader.getItems());
+		} catch (Exception e) {
+			Log.e("ITCRssReader", e.getMessage());
+		}
+		list.setAdapter(adapter);
 
-			@Override
-			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
-					long arg3) {
-				// TODO Auto-generated method stub
-				
-			}
- 
-        });
-        
-        return v;
+		return v;
 	}
-    
-    
+
 }
